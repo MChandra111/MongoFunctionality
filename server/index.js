0ad22deb01c04e04dc3b38c0d9c5407ea9cbb079
MChandra111 <hashmahesh@gmail.com>
2023-11-03 18:01:20 -0500

    Main

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
@@ -10,10 +11,21 @@ mongoose.connect(process.env.MONGODB_URI, {
   useUnifiedTopology: true,
 });
 
+app.use(cors(
+  {
+    origin: ["https://main-website-beige.vercel.app", "http://localhost:3000", "http://localhost:3000/About"],
+    methods: ["POST", "GET"],
+  }
+))
+
 app.use(express.json());
 
 const itemsRouter = require('./routes/items');
 app.use('/api/items', itemsRouter);
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
