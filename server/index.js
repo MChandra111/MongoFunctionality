30d55b7d955981ffc3bd7328a87d1ce8dab7d5b7
MChandra111 <hashmahesh@gmail.com>
2023-11-03 14:24:24 -0500

    Main

--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+console.log(process.env.MONGODB_URI);
+
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
